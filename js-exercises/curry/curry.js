7b514d475c9443a2de8e807f78d979ff87e854bd
shashank gutha <shashankgutha@gmail.com>
2021-01-20 11:18:47 +0530

    added curry functionality

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,12 +1,12 @@
 
 function curry(func){
   
- return function curriedFn(...args){
+ return function curryFn(...args){
       if(args.length>=func.length){
         return func.apply(this,args);
       }else{
         return function(...args2){
-          return curriedFn.apply(this,args.concat(args2));
+          return curryFn.apply(this,args.concat(args2));
         }
       }
  }
